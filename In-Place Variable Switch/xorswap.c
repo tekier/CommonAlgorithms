df1798adc2b5afd981d17f0e23943478559d0e6e
Tawqir Sohail <tawqir@flying-microwave.local>
2016-11-22 12:45:40 +0000

    Finished XOR swap

---
+++ In-Place Variable Switch/xorswap.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void swap(int *first, int *second)
+{
+  /*
+  first XOR second = first XOR second
+  --
+  second = (first XOR second) XOR second
+  therefore second =  first XOR (second XOR second)
+  therefore second = first XOR 0000.. = first
+  --
+  first = (first XOR second) XOR (first)
+  ...same logic as before
+  first = second
+  ********************************
+
+  anything XOR itself is 00000...
+  anything XOR 0000... is itself
+  */
+
+  *first = *first^*second;
+  *second = *first^*second;
+  *first = *first^*second;
+
+}
+
+int main()
+{
+  int first, second;
+
+  printf("\nEnter the first number: ");
+  scanf("%d", &first);
+  printf("(first) stored at address %p\n\n", &first);
+  printf("Enter the second number: ");
+  scanf("%d", &second);
+  printf("(second) stored at address %p", &second);
+
+  swap(&first, &second);
+
+  printf("\n\nThe first number is %d at: %p\n", first, &first);
+  printf("The second number is %d at: %p\n\n", second, &second);
+
+}
